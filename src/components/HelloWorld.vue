3f472e9f023a68ea35766e4edc405dc96db919d7
Vaneyck <vaneycksemeotoki@gmail.com>
2018-11-01 09:48:51 +0300

    WIP pwa support

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -3,10 +3,21 @@
     <h1>The Verses Project</h1>
     <div class="all-songs">
       <div class="song" v-for="(song, index) in songs" :key="index">
-        <img :src="song.verseImage"/>
-        <audio controls>
-          <source :src="song.mp3" type="audio/mpeg">
-        </audio>
+        <div class="level is-mobile">
+          <div class="level-item level-left">
+            <figure class="image is-128x128">
+              <img :src="song.verseImage">
+            </figure>
+          </div>
+          <div class="level-item level-left">
+            <span>{{ song.title }}</span>
+          </div>
+          <div class="level-item">
+            <button class="button" @click="play(song)">Play</button>
+            <button class="button" @click="pause()">Pause</button>
+            <button class="button" @click="addToPlayList(song)">Add to Playlist</button>
+          </div>
+        </div>
       </div>
     </div>
   </div>
@@ -19,7 +30,8 @@ export default {
   name: "HelloWorld",
   data () {
     return {
-      songs: null
+      songs: null,
+      currentSong: null
     }
   },
   mounted: function() {
@@ -36,6 +48,24 @@ export default {
     request(options, (error, response, body) => {
       this.songs = JSON.parse(body).songs;
     });
+  },
+  methods: {
+    play: function (song) {
+      // TODO Read more here https://www.binarytides.com/using-html5-audio-element-javascript/
+      if (this.currentSong) {
+        this.currentSong.pause();
+        this.currentSong = null;
+      }
+      this.currentSong = new Audio(song.mp3);
+      this.currentSong.play();
+    },
+    pause: function () {
+      this.currentSong.pause();
+      this.currentSong = null;
+    },
+    addToPlayList: function (song) {
+      alert("TODO : Add to playlist : " + song.title);
+    }
   }
 };
 </script>
